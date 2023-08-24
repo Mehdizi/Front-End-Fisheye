aa940e6aadd7b47900e86b91c9e23ffb16778328
Mehdi Tachekort <mehditachekort.dev@gmail.com>
2023-08-24 18:45:08 +0200

    last modification of previus and next button

--- scripts/features/medias.js
+++ scripts/features/medias.js
@@ -1,3 +1,5 @@
+// Part of the photographer's card (create and display)
+
 const createPhotographerPersonalCard = (photographerInfos) => {
   // On crée les deux div qui vont acceuillir la description et le portrait du photographe
   const personalCardDescription = createDomElement("div", {
@@ -48,6 +50,8 @@ const displayPhotographerPersonalCard = async () => {
   createPhotographerPersonalCard(photographerInfos);
 };
 
+// Part of the medias feed (create and display)
+
 const createMediasFeed = (media, index) => {
   // Recuperation of DOM element to insert the created HTML
   const feed = document.querySelector(".medias-feed");
@@ -74,9 +78,8 @@ const createMediasFeed = (media, index) => {
       });
     }
   };
-
   const mediaTitle = createDomElement("h2", { class: "media-title" });
-  mediaTitle.innerText = media.title + "   " + index;
+  mediaTitle.innerText = media.title;
   const likeWrapper = createDomElement("div", { class: "like-wrapper" });
   const mediaLike = createDomElement("p", { class: "media-like" });
   mediaLike.innerText = media.likes;
@@ -97,48 +100,13 @@ const displayPhotographerMediasFeed = async () => {
   const mediasFiltred = await getPhotographerMedias();
   addFilter(mediasFiltred);
   mediasFiltred.forEach((media, index) => {
-    // console.log("media :", media, "index :", index);
     createMediasFeed(media, index);
   });
-  // console.log("------------------");
 };
 
-const createLikesCounter = (photographerInfos, photographerLikes) => {
-  const mainDiv = document.querySelector(".like-counter");
-  const likesDiv = createDomElement("div", { class: "likes" });
-  const likesNumber = createDomElement("p");
-  likesNumber.innerText = `${photographerLikes}`;
-  const likeIcone = createDomElement("i", { class: "fa-solid fa-heart heart" });
-  likesDiv.append(likesNumber, likeIcone);
-  const dailyPrice = createDomElement("p", { class: "daily-price" });
-  dailyPrice.innerText = `${photographerInfos.price}€/jour`;
+// Part of the modal media (create, display)
 
-  mainDiv.append(likesDiv, dailyPrice);
-};
-
-const displayLikesCounter = async () => {
-  const photographerInfos = await getPhotographerPersonalInformations();
-  const totalLikes = await getTotalLikes();
-  createLikesCounter(photographerInfos, totalLikes);
-};
-
-const changeMedia = (medias, index) => {
-  closeModalMedia();
-
-  // If click previous and we are in the first media then take last index
-  if (index === -1) {
-    index = medias.length - 1;
-    // If click next and we are in the last media then take first index
-  } else if (index === medias.length) {
-    index = 0;
-  }
-
-  return index;
-
-  createCarousel(medias, index);
-};
-
-const mediaContent = (media) => {
+const modalMediaContent = (media) => {
   if (media.image) {
     return createDomElement("img", {
       class: "modal-media-content",
@@ -157,6 +125,21 @@ const mediaContent = (media) => {
   }
 };
 
+const modalMediaTitle = (media) => {
+  const title = createDomElement("h2", { class: "modal-media-title" });
+  title.innerText = media.title;
+  return title;
+};
+
+const displayCarousel = async (index) => {
+  // recuperation of the photographer's medias
+  const mediasFiltred = await getPhotographerMedias();
+  // add of the filter on the photographer's medias
+  addFilter(mediasFiltred);
+  // creation of the carousel with the filtred medias
+  createCarousel(mediasFiltred, index);
+};
+
 const createCarousel = (medias, index) => {
   let media = medias[index];
   let copyIndex = index;
@@ -171,70 +154,67 @@ const createCarousel = (medias, index) => {
     alt: "bouton de fermeture du media",
     onclick: "closeModalMedia()",
   });
-
   const mediaWrapper = createDomElement("div", {
     class: "modal-media-wrapper",
   });
-
   const indexNum = createDomElement("span", {
     class: "indexNum",
     innerText: `${copyIndex + 1}/${medias.length}`,
-  });
-
-  const rightArrow = createDomElement("div", {
-    class: "arrow-right",
-    events: {
-      click: () => {
-        copyIndex += 1;
-        media = medias[copyIndex];
-        mediaWrapper.innerHTML = "";
-        mediaWrapper.append(mediaContent(media));
-        indexNum.innerText = `${copyIndex + 1}/${medias.length}`;
-        // changeMedia(medias, index + 1)
-      },
-    },
   });
   const leftArrow = createDomElement("div", {
     class: "arrow-left",
     events: {
       click: () => {
-        copyIndex -= 1;
+        if (copyIndex === 0) {
+          copyIndex = medias.length - 1;
+        } else {
+          copyIndex -= 1;
+        }
         media = medias[copyIndex];
         mediaWrapper.innerHTML = "";
-        mediaWrapper.append(mediaContent(media));
-        indexNum.innerText = `${copyIndex + 1}/${medias.length}`;
-        // changeMedia(medias, index - 1)
+        mediaWrapper.append(modalMediaContent(media), modalMediaTitle(media));
       },
     },
   });
-  modal.append(cross, rightArrow, leftArrow, indexNum);
+  const rightArrow = createDomElement("div", {
+    class: "arrow-right",
+    events: {
+      click: () => {
+        if (copyIndex === medias.length - 1) {
+          copyIndex = 0;
+        } else {
+          copyIndex += 1;
+        }
+        media = medias[copyIndex];
+        mediaWrapper.innerHTML = "";
+        mediaWrapper.append(modalMediaContent(media), modalMediaTitle(media));
+      },
+    },
+  });
+  const title = createDomElement("h2");
+  title.innerText = media.title;
+  modal.append(cross, rightArrow, leftArrow);
   main.append(modal);
-
-  mediaWrapper.append(mediaContent(media));
-
+  mediaWrapper.append(modalMediaContent(media), modalMediaTitle(media));
   modal.append(mediaWrapper);
-
-  // const mediaTitle = createDomElement("h2", { class: "modal-media-title" });
-  // mediaWrapper.append(mediaContent(), mediaTitle);
 };
 
-const displayCarousel = async (index) => {
-  const mediasFiltred = await getPhotographerMedias();
-  addFilter(mediasFiltred);
-  createCarousel(mediasFiltred, index);
-  // mediasFiltred.forEach((media, index) => createCarousel(media[index]));
+// Part for the like counter
 
-  // mediasFiltred.forEach((media, index) => {
-  //   const card = document.querySelector(".photographer-media-card");
-  //   // card.addEventListener("click", console.log(index, media));
-  // });
+const displayLikesCounter = async () => {
+  const photographerInfos = await getPhotographerPersonalInformations();
+  const totalLikes = await getTotalLikes();
+  createLikesCounter(photographerInfos, totalLikes);
 };
 
-// const refreshCarousel = () => {
-//   const loadCarousel = document.querySelector(".photographer-media-card");
-//   loadCarousel.addEventListener("click", openMedia);
-//   console.log("loadCarousel");
-//   function openMedia() {
-//     displayCarousel();
-//   }
-// };
+const createLikesCounter = (photographerInfos, photographerLikes) => {
+  const mainDiv = document.querySelector(".like-counter");
+  const likesDiv = createDomElement("div", { class: "likes" });
+  const likesNumber = createDomElement("p");
+  likesNumber.innerText = `${photographerLikes}`;
+  const likeIcone = createDomElement("i", { class: "fa-solid fa-heart heart" });
+  likesDiv.append(likesNumber, likeIcone);
+  const dailyPrice = createDomElement("p", { class: "daily-price" });
+  dailyPrice.innerText = `${photographerInfos.price}€/jour`;
+  mainDiv.append(likesDiv, dailyPrice);
+};
